063104c141f4203e0defa9b3de9db19bf2fc7108
shrutiii2901 <136559812+shrutiii2901@users.noreply.github.com>
2025-01-11 22:15:28 +0530

    Time: 4 ms (94.6%), Space: 46 MB (5.4%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        int n = s.length();
+
+        if(n < k) {
+            return false;
+        }
+
+        if(n == k) {
+            return true;
+        }
+
+        int[] vec = new int[26];
+        for(char ch : s.toCharArray()) {
+            vec[ch-'a']++;
+        }
+
+        int oddFreqCharCount = 0;
+
+        for(int i = 0; i < 26; i++) {
+            if(vec[i]%2 != 0) {
+                oddFreqCharCount++;
+            }
+        }
+
+        return oddFreqCharCount <= k;
+    }
+}
