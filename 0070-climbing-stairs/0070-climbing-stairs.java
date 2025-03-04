1b640ff4b2d680a208887da0c8f3d1fc3636c9e4
shrutiii2901 <136559812+shrutiii2901@users.noreply.github.com>
2025-03-04 21:45:12 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.11%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1 || n == 2 || n == 3)
+            return n;
+        
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            
+            int temp_b = b;
+            b = c;
+            a = temp_b;
+        }
+        return c;
+    }
+}
