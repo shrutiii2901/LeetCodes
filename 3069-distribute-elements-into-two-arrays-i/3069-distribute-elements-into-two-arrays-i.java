c5075c0d7e5c7b3b7891027854e80b0900aceb7f
shrutiii2901 <136559812+shrutiii2901@users.noreply.github.com>
2026-08-20 20:59:28 +0530

    Time: 1 ms (98.08%), Space: 46.3 MB (95.43%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        int size1 = 1;
+        int size2 = 1;
+
+        arr1[0] = nums[0];
+        arr2[0] = nums[1];
+
+        for (int i = 2; i < n; i++) {
+            if (arr1[size1 - 1] > arr2[size2 - 1]) {
+                arr1[size1++] = nums[i];
+            } else {
+                arr2[size2++] = nums[i];
+            }
+        }
+
+        int[] result = new int[n];
+        int index = 0;
+
+        for (int i = 0; i < size1; i++) {
+            result[index++] = arr1[i];
+        }
+
+        for (int i = 0; i < size2; i++) {
+            result[index++] = arr2[i];
+        }
+
+        return result;
+    }
+}
