08eddf141b6f7bd283b6fc21da7234e19ef14f7d
shrutiii2901 <136559812+shrutiii2901@users.noreply.github.com>
2026-08-29 20:13:42 +0530

    Time: 147 ms (15.91%), Space: 249.7 MB (6.82%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -2,29 +2,34 @@ class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
         int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
-            while (true) {
-                int smallValue = nums[i];
-                int idx = -1;
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
 
-                for (int j = i + 1; j < n; j++) {
-                    if (Math.abs(nums[i] - nums[j]) <= limit) {
-                        if (nums[j] < smallValue) {
-                            smallValue = nums[j];
-                            idx = j;
-                        }
-                    }
-                }
+        Map<Integer, List<Integer>> group = new HashMap<>();
+        Map<Integer, Integer> groupId = new HashMap<>();
+        Map<Integer, Integer> pos = new HashMap<>();
 
-                if (idx != -1) {
-                    // Swap the values
-                    int temp = nums[i];
-                    nums[i] = nums[idx];
-                    nums[idx] = temp;
-                } else {
-                    break;
-                }
+        int id = 1;
+        group.computeIfAbsent(id, k -> new ArrayList<>()).add(sorted[0]);
+        groupId.put(sorted[0], id);
+
+        for(int i = 1; i < n; i++){
+            if(sorted[i] - sorted[i - 1] > limit){
+                id++;
             }
+
+            group.computeIfAbsent(id, k -> new ArrayList<>()).add(sorted[i]);
+            groupId.put(sorted[i], id);
+        }
+
+        // Rebuild nums using the smallest
+        // available value from its group
+        for(int i = 0; i < n; i++){
+            int grp = groupId.get(nums[i]);
+            int p = pos.getOrDefault(grp, 0);
+
+            nums[i] = group.get(grp).get(p);
+            pos.put(grp, p + 1);
         }
 
         return nums;
